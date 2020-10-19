8ce3c02097e3671fcd83cd95e123adefbd0c0adf
Alejandro Garrido Mota <garridomota@gmail.com>
2020-10-19 16:39:53 +0100

    Priority class was required by fluentd (?), now it is being deleted we dont required it anymore

--- variables.tf
+++ variables.tf
@@ -2,10 +2,6 @@ variable "dependence_prometheus" {
   description = "Prometheus module dependence - it is required in order to use this module."
 }
 
-variable "dependence_priority_classes" {
-  description = "Priority Classes dependence - it is required in order to use this module."
-}
-
 variable "elasticsearch_host" {
   description = "The elasticsearch host where logs are going to be shipped"
 }
