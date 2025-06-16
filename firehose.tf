2fe22c94c023200d98dceb3cd42e611ef6a805c4
Ky <ky.truong@digital.justice.gov.uk>
2025-06-16 22:59:47 +0100

    adding firehose to test streaming logs from fluentbit

---
+++ firehose.tf
@@ -0,0 +1,4 @@
+module "test_firehose_eks_app_logs_to_xsiam" {
+  source                    = "github.com/ministryofjustice/cloud-platform-terraform-firehose-data-stream?ref=no-loggroup"
+  destination_http_endpoint = var.cortex_xsiam_endpoint_preprod
+}
