bbfb0df51a489e12fc8254c61c26af921b547e8a
Ky <ky.truong@digital.justice.gov.uk>
2025-06-17 01:19:07 +0100

    changes to firehose module to include irsa

--- firehose.tf
+++ firehose.tf
@@ -1,4 +1,4 @@
 module "test_firehose_eks_app_logs_to_xsiam" {
-  source                    = "github.com/ministryofjustice/cloud-platform-terraform-firehose-data-stream?ref=no-loggroup"
+  source                    = "github.com/ministryofjustice/cloud-platform-terraform-firehose-data-stream?ref=fluentbit"
   destination_http_endpoint = var.cortex_xsiam_endpoint_preprod
 }
