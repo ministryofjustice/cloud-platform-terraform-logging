003ef9a24eac42b8cea9bf9844896621b939d5f3
Ky <ky.truong@digital.justice.gov.uk>
2025-06-27 14:57:53 +0100

    fixing variable

--- s3.tf
+++ s3.tf
@@ -9,7 +9,7 @@ module "s3_bucket_application_logs" {
   business_unit          = var.business_unit
   application            = var.application
   is_production          = var.is_production
-  environment_name       = var.environment
+  environment_name       = var.environment_name
   infrastructure_support = var.infrastructure_support
   namespace              = var.namespace
 } 
